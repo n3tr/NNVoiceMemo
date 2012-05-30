2c4b95bf355f5bdc59f99c254e4512d9e637532a
Jirat K <saakyz@gmail.com>
2012-05-30 16:06:14 +0700

    Initial Commit

---
+++ NNVoicePlayer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NNVoicePlayer
+//
+//  Created by n3tr on 5/30/55 BE.
+//  Copyright (c) 2555 Simpletail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NNVoicePlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NNVoicePlayer
+//
+//  Created by n3tr on 5/30/55 BE.
+//  Copyright (c) 2555 Simpletail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
